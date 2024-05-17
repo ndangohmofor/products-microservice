c2ba7687443a6caf62bde9fdb210b979f59d2bb0
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-17 17:29:52 -0400

    add product service implementation

---
+++ src/main/java/com/meufty/ws/products/service/ProductServiceImpl.java
@@ -0,0 +1,12 @@
+package com.meufty.ws.products.service;
+
+import com.meufty.ws.products.rest.CreateProductrestModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public String createProduct(CreateProductrestModel product) {
+        return null;
+    }
+}
