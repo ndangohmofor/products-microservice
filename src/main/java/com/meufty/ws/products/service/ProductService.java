329370a517ea2873e37556815da495a740e3175a
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-17 17:17:32 -0400

    create product service interface

---
+++ src/main/java/com/meufty/ws/products/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.meufty.ws.products.service;
+
+import com.meufty.ws.products.rest.CreateProductrestModel;
+
+public interface ProductService {
+    String createProduct(CreateProductrestModel product);
+}
