ead00043e2b369cf7dc09a5794c92d7dd7bb75d9
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-17 17:45:19 -0400

    use the kafkatemplate to asynchronously send the event to kafka topic without acknowledgement

--- src/main/java/com/meufty/ws/products/service/ProductServiceImpl.java
+++ src/main/java/com/meufty/ws/products/service/ProductServiceImpl.java
@@ -1,12 +1,29 @@
 package com.meufty.ws.products.service;
 
 import com.meufty.ws.products.rest.CreateProductrestModel;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    KafkaTemplate<String, ProductCreatedEvent> template;
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> template) {
+        this.template = template;
+    }
     @Override
     public String createProduct(CreateProductrestModel product) {
-        return null;
+        String productId = UUID.randomUUID().toString();
+
+        //TODO: Save product in database
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId, product.getTitle(), product.getPrice(), product.getQuantity());
+
+        template.send("product-created-event-topic", productId, productCreatedEvent);
+
+        return productId;
     }
 }
