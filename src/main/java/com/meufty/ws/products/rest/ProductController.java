d64bacee136b7cda25b2f5f914c745c6ed89cafb
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-17 17:16:04 -0400

    create a controller class to receive a post request and create a product in DB

--- src/main/java/com/meufty/ws/products/rest/ProductController.java
+++ src/main/java/com/meufty/ws/products/rest/ProductController.java
@@ -1,2 +1,17 @@
-package com.meufty.ws.products.rest;public class ProductController {
+package com.meufty.ws.products.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @PostMapping
+    public ResponseEntity<String> createProduct(@RequestBody CreateProductrestModel product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body("Product created");
+    }
 }
