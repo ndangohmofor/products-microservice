cb18146512b06bccef5b0825d74b19c97ef7bc03
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-17 16:31:25 -0400

    annotate the topic create method as a bean

--- src/main/java/com/meufty/ws/products/KafkaConfig.java
+++ src/main/java/com/meufty/ws/products/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.meufty.ws.products;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
@@ -8,8 +9,9 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
-    NewTopic createTopic(String topicName) {
-        return TopicBuilder.name(topicName)
+    @Bean
+    NewTopic createTopic() {
+        return TopicBuilder.name("product-created-event-topic")
                 .partitions(1)
                 .replicas(1)
                 .configs(Map.of("min.insync.replicas", "1"))
