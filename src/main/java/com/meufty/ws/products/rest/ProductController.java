583f697f32dac5a0d8d4262f720c039dcf015799
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-17 17:37:47 -0400

    create product generated event class

--- src/main/java/com/meufty/ws/products/rest/ProductController.java
+++ src/main/java/com/meufty/ws/products/rest/ProductController.java
@@ -1,5 +1,6 @@
 package com.meufty.ws.products.rest;
 
+import com.meufty.ws.products.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @PostMapping
     public ResponseEntity<String> createProduct(@RequestBody CreateProductrestModel product) {
-        return ResponseEntity.status(HttpStatus.CREATED).body("Product created");
+        String productId = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productId);
     }
 }
