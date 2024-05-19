30a7d8c50898e9ddba6af0924880f4118d1b82e0
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-18 23:01:39 -0400

    update the createProduct interface to throw a generic exception

--- src/main/java/com/meufty/ws/products/service/ProductService.java
+++ src/main/java/com/meufty/ws/products/service/ProductService.java
@@ -3,5 +3,5 @@ package com.meufty.ws.products.service;
 import com.meufty.ws.products.rest.CreateProductrestModel;
 
 public interface ProductService {
-    String createProduct(CreateProductrestModel product);
+    String createProduct(CreateProductrestModel product) throws Exception;
 }
