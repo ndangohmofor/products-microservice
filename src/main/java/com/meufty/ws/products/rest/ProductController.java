7144424536e34afc573514df72b1aba3efa2cebf
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-18 23:15:42 -0400

    clean up unused imports

--- src/main/java/com/meufty/ws/products/rest/ProductController.java
+++ src/main/java/com/meufty/ws/products/rest/ProductController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/products")
