07f5a7ea78c6b48883f785c81c9a83644b55680d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-19 21:56:46 -0400

    add kafka properties through the kafka config class - instead of via the properties file

--- src/main/java/com/meufty/ws/products/KafkaConfig.java
+++ src/main/java/com/meufty/ws/products/KafkaConfig.java
@@ -1,14 +1,32 @@
 package com.meufty.ws.products;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootStrapServers;
+
+    @Value("${spring.kafka.key-serializer}")
+    private String keySerializer;
+
+    Map<String, Object> producerConfigs(){
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+
+        return config;
+    }
     @Bean
     NewTopic createTopic() {
         return TopicBuilder.name("product-created-event-topic")
