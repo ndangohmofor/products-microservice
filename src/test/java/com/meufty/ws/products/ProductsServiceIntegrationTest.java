e06e67cbb045cd5fb0ccb963f84c7267379b6077
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-01 22:23:30 -0400

    create the arrange and act portions of the test

--- src/test/java/com/meufty/ws/products/ProductsServiceIntegrationTest.java
+++ src/test/java/com/meufty/ws/products/ProductsServiceIntegrationTest.java
@@ -1,10 +1,16 @@
 package com.meufty.ws.products;
 
+import com.meufty.ws.products.rest.CreateProductrestModel;
+import com.meufty.ws.products.service.ProductService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
 
 @DirtiesContext // Reset the context before each test
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -13,4 +19,26 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(properties = "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}")
 public class ProductsServiceIntegrationTest {
 
+    @Autowired
+    ProductService productService;
+
+    @Test
+    void testCreateProduct_whenGivenValidProductDetails_successfulSendsKafkaMessage() throws Exception {
+
+
+        //Arrange: Initialize objects, config mocks/stubs, prepare any input data
+        String title = "title";
+        BigDecimal price = new BigDecimal(100);
+        Integer quantity = 1;
+        CreateProductrestModel createProductrestModel = new CreateProductrestModel();
+        createProductrestModel.setTitle(title);
+        createProductrestModel.setPrice(price);
+        createProductrestModel.setQuantity(quantity);
+
+        //Act: Invoke methods under test using data from arrange section
+        productService.createProduct(createProductrestModel);
+
+
+        //Assert: Verify that methods under test executed successfully and returned expected results
+    }
 }
