115a43bb7f626d091fae72747aae01f77fe24732
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-01 21:54:15 -0400

    implementation for product service

--- src/test/java/com/meufty/ws/products/ProductsServiceIntegrationTest.java
+++ src/test/java/com/meufty/ws/products/ProductsServiceIntegrationTest.java
@@ -1,2 +1,16 @@
-package com.meufty.ws.products;public class ProductsServiceIntegrationTest {
+package com.meufty.ws.products;
+
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@DirtiesContext // Reset the context before each test
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test") // Uses application-test.yml or application-test.properties
+@EmbeddedKafka(partitions = 3, count = 3, controlledShutdown = true)
+@SpringBootTest(properties = "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}")
+public class ProductsServiceIntegrationTest {
+
 }
