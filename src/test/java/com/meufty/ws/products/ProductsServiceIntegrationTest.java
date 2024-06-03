3cc14c8bc119c26e2161fa07ab3d936a45548224
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-03 00:12:32 -0400

    create the setup method to create a method queue and add incoming messages to the queue

--- src/test/java/com/meufty/ws/products/ProductsServiceIntegrationTest.java
+++ src/test/java/com/meufty/ws/products/ProductsServiceIntegrationTest.java
@@ -1,23 +1,33 @@
 package com.meufty.ws.products;
 
+import com.meufty.ws.core.ProductCreatedEvent;
 import com.meufty.ws.products.rest.CreateProductrestModel;
 import com.meufty.ws.products.service.ProductService;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @DirtiesContext // Reset the context before each test
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -34,6 +44,20 @@ public class ProductsServiceIntegrationTest {
 
     @Autowired
     private Environment environment;
+
+    private KafkaMessageListenerContainer<String, ProductCreatedEvent> container;
+    private BlockingQueue<ConsumerRecord<String, ProductCreatedEvent>> records;
+
+    @BeforeAll
+    void setUp(){
+        DefaultKafkaConsumerFactory<String, Object> consumerFactory = new DefaultKafkaConsumerFactory<>(getConsumerProperties());
+        ContainerProperties containerProperties = new ContainerProperties(environment.getProperty("product-created-events.topic-name"));
+        container = new KafkaMessageListenerContainer<>(consumerFactory, containerProperties);
+        records = new LinkedBlockingQueue<>();
+        container.setupMessageListener((MessageListener<String, ProductCreatedEvent>) records::add);
+        container.start();
+        ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic());
+    }
 
     @Test
     void testCreateProduct_whenGivenValidProductDetails_successfulSendsKafkaMessage() throws Exception {
