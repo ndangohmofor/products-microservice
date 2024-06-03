afcaa50828eb24d548b2c3ab20aea5fefdf254f3
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-02 23:45:03 -0400

    import the right package for Environment

--- src/test/java/com/meufty/ws/products/ProductsServiceIntegrationTest.java
+++ src/test/java/com/meufty/ws/products/ProductsServiceIntegrationTest.java
@@ -2,15 +2,22 @@ package com.meufty.ws.products;
 
 import com.meufty.ws.products.rest.CreateProductrestModel;
 import com.meufty.ws.products.service.ProductService;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 @DirtiesContext // Reset the context before each test
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -20,7 +27,13 @@ import java.math.BigDecimal;
 public class ProductsServiceIntegrationTest {
 
     @Autowired
-    ProductService productService;
+    private ProductService productService;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private Environment environment;
 
     @Test
     void testCreateProduct_whenGivenValidProductDetails_successfulSendsKafkaMessage() throws Exception {
@@ -40,5 +53,14 @@ public class ProductsServiceIntegrationTest {
 
 
         //Assert: Verify that methods under test executed successfully and returned expected results
+    }
+
+    private Map<String, Object> getConsumerProperties(){
+        return Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaBroker.getBrokersAsString(),
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class,
+                ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"),
+                ConsumerConfig.);
     }
 }
