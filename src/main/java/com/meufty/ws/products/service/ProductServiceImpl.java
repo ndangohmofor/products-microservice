43ab1e54925627dfd7b32a0b6cdabf25a2ca83b9
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-02 23:59:14 -0400

    optimize imports to remove unused imports

--- src/main/java/com/meufty/ws/products/service/ProductServiceImpl.java
+++ src/main/java/com/meufty/ws/products/service/ProductServiceImpl.java
@@ -5,12 +5,11 @@ import com.meufty.ws.products.rest.CreateProductrestModel;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductServiceImpl implements ProductService {
