cade944ed2f0b3e5ee8b68f041cbb5e003ebe6eb
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-03 22:54:48 -0400

    implemented test for idempotent producer by checking the idempotent properties of the producer

--- src/test/java/com/meufty/ws/products/IdempotentProducerIntegrationTest.java
+++ src/test/java/com/meufty/ws/products/IdempotentProducerIntegrationTest.java
@@ -1,18 +1,43 @@
 package com.meufty.ws.products;
 
+import com.meufty.ws.core.ProductCreatedEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
 
 @SpringBootTest
 public class IdempotentProducerIntegrationTest {
 
+    @Autowired
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    @MockBean
+    KafkaAdmin kafkaAdmin;
+
     @Test
     void testProducerConfig_whenIdempotentEnabled_assertsIdempotentProperties() {
 
-        // TODO: Arrange
+        // Arrange
+        ProducerFactory<String, ProductCreatedEvent> producerFactory = kafkaTemplate.getProducerFactory();
 
-        // TODO: Act
+        // Act
+        Map<String, Object> config = producerFactory.getConfigurationProperties();
 
-        // TODO: Assert
+        // Assert
+        Assertions.assertTrue((Boolean) config.get(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG));
+        Assertions.assertTrue("all".equals(config.get(ProducerConfig.ACKS_CONFIG)));
+        if (config.containsKey(ProducerConfig.RETRIES_CONFIG)) {
+            Assertions.assertTrue(
+                    Integer.parseInt(config.get(ProducerConfig.RETRIES_CONFIG).toString()) > 0
+            );
+        }
     }
 }
