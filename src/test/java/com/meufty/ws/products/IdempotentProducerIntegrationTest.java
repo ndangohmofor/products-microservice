6fda135f88bcb067a8cc2b15fde14f5f3ac6cc6e
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-03 22:41:00 -0400

    add class for testing kafka producer idempotency

--- src/test/java/com/meufty/ws/products/IdempotentProducerIntegrationTest.java
+++ src/test/java/com/meufty/ws/products/IdempotentProducerIntegrationTest.java
@@ -1,2 +1,18 @@
-package com.meufty.ws.products;public class IdempotentProducerIntegrationTest {
+package com.meufty.ws.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IdempotentProducerIntegrationTest {
+
+    @Test
+    void testProducerConfig_whenIdempotentEnabled_assertsIdempotentProperties() {
+
+        // TODO: Arrange
+
+        // TODO: Act
+
+        // TODO: Assert
+    }
 }
